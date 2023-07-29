c061b91b544c4eb3339184798b3b57c3d28c43cc
Aleticod <193837@unsaac.edu.pe>
2023-07-29 12:56:26 -0500

    Add components for icons

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,19 +1,24 @@
 import './TodoItem.css'
+import { DeleteIcon } from './DeleteIcon';
+import { CompleteIcon } from './CompleteIcon';
+
 
 // Creando un nuevo componente
 // Este componente lo insertaremos dentro del componente App
 function TodoItem({ text, completed, onComplete, onDelete }) {
     return (
       <li className="TodoItem">
-        <span
+        <CompleteIcon />
+        {/* <span
           className= {`Icon Icon-check ${ completed && "Icon-check--active"}`}
           onClick={onComplete}
-        >V</span>
+        >V</span> */}
         <p className={`TodoItem-p ${ completed && "TodoItem-p--complete"}`}>{text}</p>
-        <span
+        <DeleteIcon />
+        {/* <span
           className="Icon Icon-delete"
           onClick={onDelete}
-        >X</span>
+        >X</span> */}
       </li>
     );
 }
